0a77dd047bac46520b754f74bba4cb66afa0fce9
Manuelz00 <mjzumba.2@sudamericano.edu.ec>
2023-11-30 19:00:32 -0600

    Initial commit

---
+++ deber09.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function Jugador (nickname, vidas, carta){
+    this.nickname = nickname;
+    this.vidas = vidas;
+    this.carta = carta;
+    this.disminuirVida = function (){
+
+    }
+    //Función para que devuelva el nombre y las vidas
+}
